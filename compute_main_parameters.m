ea0a4439950f66684daeacffc6673abf1eba7801
Javier Almonacid <almonacid.javier@gmail.com>
2023-12-27 01:57:18 -0300

    Turned compute_main_parameters into a function that takes as input the operating parameters of the device and other guessed parameters

--- compute_main_parameters.m
+++ compute_main_parameters.m
@@ -12,10 +12,13 @@
 %
 % Date: August 30, 2023
 %
-clear; clc; close all;
+function out = compute_main_parameters(delta_p, ...
+                                       qt, ...
+                                       Rcal_s, ...
+                                       bubble_radius)
 
 % Compute [w], hereafter called wmax
-delta_p = 1e+03;     % Pa (guessed)
+%delta_p = 2500;     % Pa (guessed)
 h = 1;               % m
 a = 1e-4;            % m
 mu0_water = 8.9e-04; % Pa s
@@ -31,16 +34,16 @@ g = 9.81; % m/s^2
 Fcal = wmax / (delta * sqrt(g*h));
 
 % Compute Da_t
-qt = 10; % Guessed
+%qt = 1e-02; % Guessed
 cd_sat_hydrogen = 7.9e-4; % mol/L
 Da_t = delta * h * qt / (D_hydrogen * cd_sat_hydrogen);
 
 % Compute Da_s
-Rcal_s = 10; % Guessed
+%Rcal_s = 1e-10; % Guessed
 Da_s = delta * h * Rcal_s / (D_hydrogen * cd_sat_hydrogen);
 
 % Compute epsilon;
-bubble_radius = 1e-6; % Guessed(ish), literature says 1e-4
+%bubble_radius = 1e-6; % Guessed
 epsilon = bubble_radius / a;
 
 % Compute lambda
@@ -54,7 +57,7 @@ lambda = (3/2) * (1/(delta^2*Re)) * (cd_sat_hydrogen/cb_max) * ...
          ((2+3*mub_hydrogen/mu0_water)/(1-mub_hydrogen/mu0_water));
 
 % Compute xi
-beta = 10; % Guessed
+beta = 1/cd_sat_hydrogen; % Guessed
 xi = beta*cd_sat_hydrogen;
 
 % Compute Fr
@@ -62,3 +65,13 @@ Fr = delta * Fcal;
 
 % Display results
 table(wmax,Pcal,Fcal,Da_t,Da_s,lambda,xi,epsilon,Re,Pe,Fr)
+
+if nargout > 0
+    out.reduced_Pe = Pcal;
+    out.reduced_Fr = Fcal;
+    out.Da_t = Da_t;
+    out.Da_s = Da_s;
+    out.lambda = lambda;
+    out.xi = xi;
+    out.epsilon = epsilon; 
+end
