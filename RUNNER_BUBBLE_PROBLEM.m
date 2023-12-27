ac37783ccc31173e35e67f51f5c1ca7bbe76e9f1
Javier Almonacid <almonacid.javier@gmail.com>
2023-12-27 01:58:05 -0300

    Modified script for the bubble problem to consider the parameters from the report

--- RUNNER_BUBBLE_PROBLEM.m
+++ RUNNER_BUBBLE_PROBLEM.m
@@ -12,22 +12,28 @@
 %
 % Date: August 29, 2023
 %
-clear; clc; close all;
+clear; close all;
 
 %% Single computation
 
+delta_p = 2500;
+qt = 1e-02;
+Rcal_s = 1e-10;
+bubble_radius = 1e-06;
+coef = compute_main_parameters(delta_p, qt, Rcal_s, bubble_radius);
+
 % Input parameters
-reduced_Pe = 500;  % Reduced Peclet number
-reduced_Fr = 4;
 dx = 0.01/4;        % Spatial mesh size
-Da_t = 500;          % Damkohler number (dissolved gas)
-Da_s = 1000;          % Damkohler number (bubbles)
-lambda = 1;
-xi = 15;
-epsilon = 1e-3;
+reduced_Pe = coef.reduced_Pe;  % Reduced Peclet number
+reduced_Fr = coef.reduced_Fr;  % Reduced Froude number
+Da_t = coef.Da_t;              % Damkohler number (dissolved gas)
+Da_s = coef.Da_s;              % Damkohler number (bubbles)
+lambda = coef.lambda;
+xi = coef.xi;
+epsilon = coef.epsilon;
 
 % Compute non-dimensional solution
-sol_CD = find_concentration_dissolved_gas(reduced_Pe,dx,Da_t,Da_s,lambda,xi);
+sol_CD = find_concentration_dissolved_gas(reduced_Pe,dx,Da_t,Da_s,lambda,xi,epsilon);
 max_CD = max(max(sol_CD.C));
 min_CD = min(min(sol_CD.C));
 
@@ -47,7 +53,7 @@ subplot(1,2,1)
 surf(sol_CD.X,sol_CD.Z,sol_CD.C,'EdgeColor','none')
 view([0 90])
 colorbar
-caxis([min_colorbar,max_colorbar])
+%caxis([min_colorbar,max_colorbar])
 colormap('jet')
 
 xlabel('$\hat{x}$','Interpreter','latex','FontSize',16)
@@ -60,12 +66,13 @@ subplot(1,2,2)
 surf(sol_CD.X,sol_CD.Z,sol_CB.C,'EdgeColor','none')
 view([0 90])
 colorbar
-caxis([min_colorbar,max_colorbar])
+%caxis([min_colorbar,max_colorbar])
 colormap('jet')
 xlabel('$\hat{x}$','Interpreter','latex','FontSize',16)
 ylabel('$\hat{z}$','Interpreter','latex','FontSize',16)
 title(['$\widehat{c}_b$ ($\mathcal{F} =$ ',num2str(reduced_Fr), ...
        ', $Da_s =$ ', num2str(Da_s), ')'], ...
        'Interpreter', 'latex', 'FontSize', 14)
-
+set(gcf,'Position',[479 172 1220 388])
+exportgraphics(gcf,'figs/with_bubbles_full.png', 'Resolution', 300)
 
